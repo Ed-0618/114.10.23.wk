0e5d16a9abdd275e0d8603e80f47304c1f50f670
Ed-0618 <wasd940618@gamil.com>
2025-10-23 12:55:26 +0800

    second

---
+++ c9.php
@@ -0,0 +1,16 @@
+<?php
+$arr = array(
+    "sid" => "C112181153",
+    "name" => "許一德",
+    "shape" => "@@",
+    100
+);
+
+// foreach ($arr as $key => $value) {
+//     echo "$key => $value";
+// };
+
+foreach ($arr as $key => $value)
+    echo "$key => $value <BR> ";
+
+?>
